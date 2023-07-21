dae28fb9a42a72ec27b5ca5428a753a44c757812
= <=>
2023-07-22 09:21:06 +1000

    started making the constructors for the reader and writer classes

--- a1-startup/copier_files/reader.cpp
+++ a1-startup/copier_files/reader.cpp
@@ -5,6 +5,11 @@
 #include "reader.h"
 #include <functional>
 
-reader::reader(const std::string& name, writer& mywriter) {}
+reader::reader(const std::string& name, writer& mywriter):
+    thewriter(*new writer(name))
+{
+    
+}
 
-void reader::run() {}
+void reader::run() {
+}
--- a1-startup/copier_files/writer.cpp
+++ a1-startup/copier_files/writer.cpp
@@ -8,7 +8,11 @@
 /**
  * provide your implementation for the writer functions here
  **/
-writer::writer(const std::string& name) {}
+writer::writer(const std::string& name):
+    queue(*new std::deque<std::string>())
+{
+    out.open(name, std::ios::binary);
+}
 
 void writer::run() {}
 
