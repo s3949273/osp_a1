703b37d3c127fceab333c388a8e5a645f13c2676
= <=>
2023-07-21 21:17:26 +1000

    hello world

--- a1-startup/copier_files/main.cpp
+++ a1-startup/copier_files/main.cpp
@@ -5,9 +5,13 @@
 #include "reader.h"
 #include "writer.h"
 #include <cstdlib>
+#include <iostream>
+using std::cout;
+using std::endl;
 /* global variables if needed go here */
 int main(int argc, char** argv) {
     /* check command line arguments */
     /* load the file and copy it to the destination */
+    cout<<"Hello world"<<endl;
     return EXIT_SUCCESS;
 }
