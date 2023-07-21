dfa82a93f3fd3199ad36a6484113c7628f9a1e65
= <=>
2023-07-22 09:21:27 +1000

    removed hello world

--- a1-startup/copier_files/main.cpp
+++ a1-startup/copier_files/main.cpp
@@ -8,10 +8,13 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+
+
 /* global variables if needed go here */
 int main(int argc, char** argv) {
     /* check command line arguments */
+    
     /* load the file and copy it to the destination */
-    cout<<"Hello world"<<endl;
+    
     return EXIT_SUCCESS;
 }
