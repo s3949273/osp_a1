524c47863f0bdb95a8bddf0a64716434b23b4da1
s3949273 <103175781+s3949273@users.noreply.github.com>
2023-08-14 00:09:24 +1000

    Update reader.cpp

--- a1-startup/mtcopier_files/reader.cpp
+++ a1-startup/mtcopier_files/reader.cpp
@@ -1,76 +1,68 @@
-/**
- * startup code provided by Paul Miller for COSC1114 - Operating Systems
- * Principles
- **/
-#include "reader.h"
 
-#include "writer.h"
+    /**
+     * startup code provided by Paul Miller for COSC1114 - Operating Systems
+     * Principles
+     **/
+    #include "reader.h"
+    #include <unistd.h>
+    #include "writer.h"
 
-/**
- * implement the functions needed for this class
- **/
-std::ifstream reader::in;
-unsigned reader::threads_to_spawn = 0;
-pthread_cond_t reader::cond;
-pthread_mutex_t reader::lock;
+    /**
+     * implement the functions needed for this class
+     **/
+    std::ifstream reader::in;
+    pthread_t reader::r_thread;
+    pthread_mutex_t* reader::r_mutex;
+    void reader::init(const std::string& name){
+        in.open(name);
+    }
 
-void reader::init(const std::string& name) {
-    in.open(name);
-}
-void reader::set_threads_to_spawn(int num_threads){
-    reader::threads_to_spawn = num_threads;
-}
-
-void *PrintHello(void *threadid) {
-   long tid;
-   tid = (long)threadid;
-   std::cout << "Hello World! Thread ID, " << tid << std::endl;
-   pthread_exit(NULL);
-}
-
-
-void reader::run() {
-    pthread_t threads[threads_to_spawn-1];
-    std::string line = "";
-    lock =  PTHREAD_MUTEX_INITIALIZER;
-    size_t counter = 0;
-    while(!in.eof()){
-        for(size_t x = 0; x<threads_to_spawn;x++){
-            counter = x;
-            if(!in.eof()){
-                //just check agian whether we've reached eof
-            pthread_create(&threads[x],NULL,runner, (void* ) &in);
-            }else{
-                break;
-            }
+    void reader::join(){
+        int ret = pthread_join(r_thread, NULL);
+        if(ret !=0 ){
+            std::cout<<"Err: "<<strerror(ret)<<std::endl; 
+        }else{
+            std::cout<<"finished joining reader threads"<<std::endl;
         }
-        for(size_t x = 0; x<counter;x++){
-            //we use counter here and not threads_to_spawn in case we break out of
-            //the for loop above prematurely due to end of file, this way we won't
-            //be calling join() on threads that aren't being used
-            pthread_join(threads[x], NULL);
+        if(in.is_open()){
+            in.close();
+            std::cout<<"closed input file"<<std::endl;
         }
     }
-    
-    //destroy condition, and mutex
-    pthread_cond_destroy(&cond);
-    pthread_mutex_destroy(&lock);
-    std::cout<<"finished reading successfully"<<std::endl;
-}
 
-void* reader::runner(void* arg) {
-        pthread_mutex_lock(&lock);
-        std::string line = "";
-        std::ifstream* s = (std::ifstream*)arg;
-        pthread_cond_wait(&cond, &lock);
-        getline(*s, line);
-        // std::cout<<line<<std::endl;
-        writer::append(line);
-        if(pthread_mutex_unlock(&lock)!= 0){
-            std::cout<<"something went wrong when unlocking the mutex"<<std::endl;
-        };     
-        if(pthread_cond_signal(&cond) != 0){
-            std::cout<<"something went wrong when sending the condition signal"<<std::endl;
-        };
+    void reader::run() {
+        if(in.is_open()){
+            if(pthread_create(&r_thread, NULL, runner, NULL)!=0){
+                std::cout<<"something went wrong creating the thread"<<std::endl;
+            }
+        }else{
+            std::cout<<"input file was not open"<<std::endl;
+        }
+
+    }
+
+    void* reader::runner(void* arg) {   
+        while(true){    
+            std::string line = "";
+            if(pthread_mutex_lock(r_mutex)!=0){
+                std::cout<<"something went wrong locking"<<std::endl;
+            }
+            if(getline(in, line)){
+                writer::append(line);
+                pthread_cond_signal(&writer::line_ready);
+            }else{
+                if(!writer::finished){
+                    writer::setfinished();
+                }
+                if(pthread_mutex_unlock(r_mutex)!=0){
+                    std::cout<<"something went wrong unlocking reader lock"<<std::endl;
+                }
+                break;
+            }
+            if(pthread_mutex_unlock(r_mutex)!=0){
+                std::cout<<"something went wrong unlocking"<<std::endl;
+            }
+        }
+        pthread_exit(NULL);
         return nullptr; 
     }
