9507abe7b7b1d853f4c3b6b4cb2fc6878785f5dd
s3949273 <103175781+s3949273@users.noreply.github.com>
2023-08-14 00:13:14 +1000

    Update main.cpp

--- a1-startup/mtcopier_files/main.cpp
+++ a1-startup/mtcopier_files/main.cpp
@@ -3,68 +3,82 @@
  * Principles
  **/
 #include "reader.h"
+#include <unistd.h>
 #include "writer.h"
 #include <cstdlib>
-#include <ctime>
 /**
  * these need to be global variables as you'll need handle cleaning them up in
  *cleanup which will automatically be called on program exit
  **/
 reader* readers;
 writer* writers;
+pthread_mutex_t shared_mutex;
 
 
 void cleanup() {
     /**
      * perform any cleanup you need to do here for global pointers
      **/
-    delete readers;
-    delete writers;
+    if(readers!=nullptr){
+        delete[] readers;    
+    }
+    if (writers != nullptr){
+        delete[] writers;
+    }
+    pthread_mutex_destroy(&shared_mutex);
 }
+
 
 int main(int argc, char** argv) {
     atexit(cleanup);
-    
-
     /**
      * check command line arguments
-     **/ if(argc != 4){
-        //incorrect amount of command line arguments
-        std::cout<<"Sorry did not get the expected command line arguments"<<std::endl; 
-        std::cout<<"expected arguments are: number of threads to spawn, input filepath and output filepath separated by spaces"<<std::endl;
-        std::cout<<"expected example: ./copier 10 input_file output_file"<<std::endl;
-    }else{
-        //correct amount of command line arguments
-        //make sure to do some input validation
-
-        std::cout<<"read/writing with "<<argv[1]<<" number of threads"<<std::endl;
-
-        readers->init((std::string)argv[2]);
-        std::cout<<"using: "<<argv[1]<<" threads"<<std::endl;
-        readers->set_threads_to_spawn(std::atoi(argv[1]));
-        std::clock_t start = std::clock();
-        readers->run();
-        double duration =( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-        std::cout<<"duration was: "<<duration<<std::endl;
+     **/
+    if(argc!= 4){
+        std::cout<<"incorrect amount of command line arguments, expected 3, got: "<<argc-1<<std::endl;
     }
-
-
-    /**
-     * initiliaze the reader and writer classes
-     **/
-    /**
-     * initialize the running of each thread. Note you just call run() on each
-     * object here, you'll call pthread_create itself in the run function.
-     **/
-
-
-
-
-    /**
-     *
-     * have loop here waiting for the threads to bomplete. Please see
-     * section on avoiding busy waiting on the assignment specification to see
-     * what need to be done here
-      **/
+    else{
+        try{
+            /**
+             * process command line arguments
+             **/
+            const unsigned int num_threads = std::stoi(argv[1]);
+              /**
+             * initiliaze the reader and writer classes
+             **/
+            readers = new reader[num_threads];
+            writers = new writer[num_threads];
+            readers->init(argv[2]);
+            writers->init(argv[3]);
+            
+            pthread_mutex_init(&shared_mutex,NULL);
+            readers->r_mutex = &shared_mutex;
+            writers->w_mutex = &shared_mutex;
+            
+            /**
+             * initialize the running of each thread. Note you just call run() on each
+             * object here, you'll call pthread_create itself in the run function.
+             **/
+            std::clock_t start = std::clock();
+            for(size_t x = 0; x<num_threads; x++){
+                readers[x].run();
+                writers[x].run();
+            }
+            
+            /**
+             *
+             * have loop here waiting for the threads to bomplete. Please see
+             * section on avoiding busy waiting on the assignment specification to see
+             * what need to be done here
+            **/
+            readers->join();
+            writers->join();
+            
+            std::cout<<"duration was: "<<( std::clock() - start ) / (double) CLOCKS_PER_SEC<<std::endl;
+        }catch(...){
+            std::cout<<"The command line argument which was supposed to contain the number of threads did not contain any numbers"<<std::endl;
+        }
+    }
+    std::cout<<"end of program, have a nice day"<<std::endl;
     return EXIT_SUCCESS;
 }
